e4a71cc8e4c2998ef5da198ad6cc4bcc365956f8
Kirill <ekoroteev27@yandex.ru>
2020-12-08 17:55:13 +0300

    design

---
+++ app.js
@@ -0,0 +1,18 @@
+let january = document.querySelector('.january')
+let february = document.querySelector('.february')
+let march = document.querySelector('.march')
+let april = document.querySelector('.april')
+let may = document.querySelector('.may')
+let june = document.querySelector('.june')
+let july = document.querySelector('.july')
+let august = document.querySelector('.august')
+let september = document.querySelector('.september')
+let october = document.querySelector('.october')
+let november = document.querySelector('.november')
+let december = document.querySelector('.december')
+
+let month = ['744','672','744','720','744','720','744','744','720','744','720','744']
+
+function monthHour() = {
+
+}
