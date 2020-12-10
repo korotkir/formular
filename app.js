e7cb53c06b5b728b86cf31ed611b570751e1c2e3
Kirill <ekoroteev27@yandex.ru>
2020-12-10 17:46:43 +0300

    process

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ let october = document.querySelector('.october')
 let november = document.querySelector('.november')
 let december = document.querySelector('.december')
 
-let month = ['744','672','744','720','744','720','744','744','720','744','720','744']
 
-function monthHour() = {
+let month = [january, february, march, april, may, june, july, august, september, october, november, december]
+let hour = ['744','672','744','720','744','720','744','744','720','744','720','744']
 
+function monthHour() {
+  for(i = 0; i < month.length; i++) {
+    month[i].innerHTML = hour[i]
+  } 
 }
+
+monthHour()
